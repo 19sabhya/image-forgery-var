c93cc92aba6d7a8485a550e71abf44492fbe6571
Sabhya A. Lokhande <64120740+19sabhya@users.noreply.github.com>
2021-04-30 15:17:21 +0530

    Update detect.py

--- Image-Forgery-Detection-master/src/detect.py
+++ Image-Forgery-Detection-master/src/detect.py
@@ -26,6 +26,6 @@ if __name__ == '__main__':
     im_str = args[0]
     
     print('\nRunning double jpeg compression detection...')
-    # cv2.imshow('img', im_str)
+    cv2.imshow('img', args[0])
     
     
