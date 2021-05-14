45f95a9d639bbcf1cda3ef5972df9706f347c9d0
Sabhya A. Lokhande <64120740+19sabhya@users.noreply.github.com>
2021-05-14 18:31:31 +0530

    Update create_dataset_file.py

--- Image-Forgery-Detection-master/src/create_dataset_file.py
+++ Image-Forgery-Detection-master/src/create_dataset_file.py
@@ -2,19 +2,25 @@ import numpy as np
 import sys
 import os
 import random
+import detect
 
-
-def create_dataset_file(myDir):
-	data_csv_file = open("../images/tpImages.csv", "w")
-	format_ = ['.JPG' , '.jpg', 'jpeg', '.png', '.tiff', '.TIFF', '.Tiff', '.Tif', '.TIF', '.tif', '.bmp']
-	
-	for root, dirs, files in os.walk(myDir, topdown=False):
-		for name in files:
-			for type_ in format_:
-				if name.endswith(type_):
-					fullName = os.path.join(root, name)
-          print('f: ', fullName)
-					data_csv_file.write("%s\n" % (fullName))
-					break
-  			
-	data_csv_file.close()
+if __name__ == '__main__':
+  
+  data_csv_file = open("../images/tpImages.csv", "w")
+  format_ = ['.JPG' , '.jpg', 'jpeg', '.png', '.tiff', '.TIFF', '.Tiff', '.Tif', '.TIF', '.tif', '.bmp']
+  print('hi')
+  for root, dirs, files in os.walk('..//images//', topdown=False):
+    for name in files:
+      for type_ in format_:
+        if name.endswith(type_):
+          fullName = os.path.join(root, name)
+          data_csv_file.write("%s\n" % (fullName))
+          break
+  
+  data_csv_file.close()
+  tpImagePaths = open('..//images//tpImages.csv', 'r').readlines()
+  tpImagePaths = sorted(tpImagePaths)
+  for imagePath in tpImagePaths:
+    a = imagePath.strip().rsplit('//', 1)
+    print(a[1])
+    detect.detect(a[1])
