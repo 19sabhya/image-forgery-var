144b38db12638d447f7728e35e3a8965e5f55a0c
Sabhya A. Lokhande <64120740+19sabhya@users.noreply.github.com>
2021-05-14 18:32:09 +0530

    Update detect.py

--- Image-Forgery-Detection-master/src/detect.py
+++ Image-Forgery-Detection-master/src/detect.py
@@ -9,22 +9,24 @@ import ela_extractor
 
 from optparse import OptionParser
 
-if __name__ == '__main__':
+def detect(cmf):
+    cmf= 
     # copy-move parameters
-    cmd = OptionParser("usage: %prog image_file [options]")
-    cmd.add_option('', '--imauto', help='Automatically search identical regions. (default: %default)', default=1)
-    cmd.add_option('', '--imblev',help='Blur level for degrading image details. (default: %default)', default=8)
-    cmd.add_option('', '--impalred',help='Image palette reduction factor. (default: %default)', default=15)
-    cmd.add_option('', '--rgsim', help='Region similarity threshold. (default: %default)', default=5)
-    cmd.add_option('', '--rgsize',help='Region size threshold. (default: %default)', default=1.5)
-    cmd.add_option('', '--blsim', help='Block similarity threshold. (default: %default)',default=200)
-    cmd.add_option('', '--blcoldev', help='Block color deviation threshold. (default: %default)', default=0.2)
-    cmd.add_option('', '--blint', help='Block intersection threshold. (default: %default)', default=0.2)
-    opt, args = cmd.parse_args()
+    cmf = OptionParser("usage: %prog image_file [options]")
+    cmf.add_option('', '--imauto', help='Automatically search identical regions. (default: %default)', default=1)
+    cmf.add_option('', '--imblev',help='Blur level for degrading image details. (default: %default)', default=8)
+    cmf.add_option('', '--impalred',help='Image palette reduction factor. (default: %default)', default=15)
+    cmf.add_option('', '--rgsim', help='Region similarity threshold. (default: %default)', default=5)
+    cmf.add_option('', '--rgsize',help='Region size threshold. (default: %default)', default=1.5)
+    cmf.add_option('', '--blsim', help='Block similarity threshold. (default: %default)',default=200)
+    cmf.add_option('', '--blcoldev', help='Block color deviation threshold. (default: %default)', default=0.2)
+    cmf.add_option('', '--blint', help='Block intersection threshold. (default: %default)', default=0.2)
+    opt, args = cmf.parse_args()
     if not args:
-        cmd.print_help()
+        cmf.print_help()
         sys.exit()
     im_str = args[0]
+    print(im_str)
     
     startTimestamp = time.time()
     
